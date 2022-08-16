491416cf0c9831a1c65a8b8bd9e426319ccc113a
SuTuncel <83240777+SuTuncel@users.noreply.github.com>
2022-08-16 11:55:54 +0300

    oop

--- oop/index.js
+++ oop/index.js
@@ -252,7 +252,6 @@ var cards = [ new Card("A", "Hearts"),
 ];
 
 cards.forEach(card => console.log(`${card.value} ${card.suit}`));
-*/
 // error handling
 
 try{
@@ -270,3 +269,36 @@ catch(error){
 finally{
     console.log("This almays executes");
 }
+
+// setTimeout = invokes a function after a number of milliseconds
+
+let item = "cryptocurrency"; 
+let price = 420.87;
+
+let timer1 = setTimeout(firstMessage, 3000, item, price);
+let timer2 = setTimeout(secondMessage, 6000);
+let timer3 = setTimeout(thirdMessage, 9000);
+
+function firstMessage(item, price){
+    alert(`Buy this ${item} for $${price}`);
+}
+
+function secondMessage(){
+    alert(`This is not a scam!`);
+}
+
+function thirdMessage(){
+    alert(`DO ITTT!`);
+}
+
+document.getElementById("myButton").onclick = function(){
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    clearTimeout(timer3);
+    alert(`thanks for buying`);
+}
+*/
+
+//setInterval()
+
+
